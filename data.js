5dd4bdc771383a24a20042b85a5427e78477a306
SashaPlavi <sasha.bingulac@gmail.com>
2019-08-19 12:46:10 +0200

    have moon pictures

--- data.js
+++ data.js
@@ -1,7 +1,22 @@
-//import { main } from "./main";
+import { main } from "./main.js";
 
-function fetchg() {
-  console.log("cao 2!!!");
+function fetchg(call) {
+  //console.log("cao 2!!!");
+  const demokey = "DEMO_KEY";
+
+  const date =
+    new Date().getFullYear().toString() +
+    "-" +
+    new Date().getMonth().toString() +
+    "-" +
+    new Date().getDate().toString();
+  console.log(date);
+  const url = `https://api.nasa.gov/planetary/apod?date=${date}&&api_key=${demokey}`;
+  fetch(url)
+    .then(reponse => reponse.json())
+    .then(res => {
+      call(res);
+    });
 }
 
 export { fetchg };
--- main.js
+++ main.js
@@ -2,16 +2,30 @@ import { fetchg } from "./data.js";
 
 function main() {
   console.log("cao drugari");
-  const button = document.createElement("button");
-  const place = document.querySelector(".container");
-  button.innerHTML = "show me galaxy!!!";
-  button.addEventListener("click", fretchGalaxy);
-  place.appendChild(button);
-}
+  createbutton();
+  function createbutton() {
+    const button = document.createElement("button");
+    const place = document.querySelector(".container");
+    button.innerHTML = "show me galaxy!!!";
+    button.addEventListener("click", fretchGalaxy);
+    place.appendChild(button);
+  }
 
-function fretchGalaxy() {
-  console.log("galaxy");
-  fetchg();
-}
+  function fretchGalaxy() {
+    console.log("galaxy");
+    fetchg(getdata);
+  }
 
+  function getdata(data) {
+    const url = data.url;
+    const text = data.explanation;
+    console.log(url);
+    console.log(text);
+
+    const place = document.querySelector(".container");
+    place.innerHTML = `<img src ='${url}'> <p>${text}</p>`;
+    const img = document.querySelector("img");
+    img.addEventListener("click", createbutton);
+  }
+}
 export { main };
