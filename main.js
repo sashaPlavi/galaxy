a49ad731a27a27361ce793f1b43ff3693ca4bd73
SashaPlavi <sasha.bingulac@gmail.com>
2019-08-19 21:35:53 +0200

    everything works

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 import { fetchg } from "./data.js";
 
 function main() {
+  const place = document.querySelector(".container");
   console.log("cao drugari");
   createbutton();
   function createbutton() {
     const button = document.createElement("button");
-    const place = document.querySelector(".container");
-    button.innerHTML = "show me galaxy!!!";
+    //const place = document.querySelector(".container");
+    button.innerHTML = "show me galaxy !!!";
     button.addEventListener("click", fretchGalaxy);
     place.appendChild(button);
   }
@@ -18,14 +19,24 @@ function main() {
 
   function getdata(data) {
     const url = data.url;
-    const text = data.explanation;
     console.log(url);
-    console.log(text);
+    // console.log(text);
 
-    const place = document.querySelector(".container");
-    place.innerHTML = `<img src ='${url}'> <p>${text}</p>`;
+    //const place = document.querySelector(".container");
     const img = document.querySelector("img");
-    img.addEventListener("click", createbutton);
+    place.innerHTML = `<img src ='${url}'>  `;
+    // img.addEventListener("click", createbutton);
+    const button = document.createElement("button");
+    // const place = document.querySelector(".container");
+    button.innerHTML = "show me description of picture !!!";
+    button.addEventListener("click", adddescription);
+    place.appendChild(button);
+    function adddescription() {
+      const text = data.explanation;
+      const desctiption = document.createElement("p");
+      desctiption.innerHTML = `${text}`;
+      place.appendChild(desctiption);
+    }
   }
 }
 export { main };
